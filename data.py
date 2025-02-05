c25a4668187827b0568d19dcda8b8fee2f15475c
DavidHashBrownQiu <longhutch@gmail.com>
2025-02-05 10:16:01 -0500

    Unit2ProgressStringsAndStuffz

---
+++ data.py
@@ -0,0 +1,24 @@
+#integers - whole numbers and floats - decimals
+"""x = 3
+y = float(3)
+print(x,y)"""
+
+#iterations allow us to scroll through the 
+#values while printing the variables itself does the entire thing
+values = [1,2.23,5,7,2,30,15]
+"""print(values)
+for i in values:
+    print(i)"""
+
+#prints out values from the variable in terms of their placement. 7 is out of range because we don't have a value in the 7th placement.
+"""print(values[7])"""
+
+#strings are individual letters that make up a word.
+x = "this is a thing"
+y = x.split()
+z = y[0]
+print(y)
+print(z)
+#split is basically what we did for the integer values where it takes one word from the string.
+
+
---
+++ datatypes.py
@@ -0,0 +1,34 @@
+#Strings are for representing characters, names, words, etc...
+#name = "Deivid"
+
+#integer represents whole numbers
+#age: 20
+
+#Floats represent decimals
+#wallet = 5000000000000000
+
+#boolean represents true or false, used in evaluations
+#graduated = False
+
+#def add(x,y):
+    #print(x + y)
+
+#use chatgpt for error messages in case you don't understand.
+
+#input asks the user a questions and stores their response as a value
+
+#bill = float(input("what was the bill?"))
+
+#add(4, bill)
+
+#lists
+students = ["Joanna", "Deivid", "David", "other David", "Ethan"]
+#similar to saying for i in range(5) print(students[i)])
+for student in students:
+    print(student)
+
+moneys = [1,2,3,4,5]
+total = 0
+    for money in moneys
+        total = total + money
+    print(total)
